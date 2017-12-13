7d0589b914ba65ae1d8b9290a7bae27107b556cf
max <maxim21214@gmail.com>
2017-12-14 00:27:02 +0700

    typo fix

--- lib/inbound_event_socket_listener.rb
+++ lib/inbound_event_socket_listener.rb
@@ -5,7 +5,7 @@ require_relative 'event_processors/channel_park_processor'
 
 class InboundEventSocketListener < FSR::Listener::Inbound
   def before_session
-    add_event(:CHANNEL_PARK) do |e|
+    add_event(:CHANNEL_PARK) do |event|
       FSR::Log.info "*** Process CHANNEL_PARK event ***"
       res = EventProcessors::ChannelParkProcessor.new.process event
       FSR::Log.info "Response: #{res}"
