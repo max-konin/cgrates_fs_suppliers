623a7c7ad9348d18e69d7b0404ca847503fc9253
max <maxim21214@gmail.com>
2017-12-14 11:08:53 +0700

    config load before starting server

--- bin/server.rb
+++ bin/server.rb
@@ -1,6 +1,6 @@
 require_relative "../lib/inbound_event_socket_listener"
 
-config = Config.instance
+config = Config.instance.load
 FSR.start_ies!(InboundEventSocketListener, host: config.freeswitch_host,
                                            port: config.freeswitch_port,
                                            auth: config.freeswitch_auth)
